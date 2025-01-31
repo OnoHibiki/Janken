bbae741a77ba232fe36e5b2d3db0071df2c9f3a0
Hibiki Ono <onohibiki0427@gmail.com>
2025-01-31 18:16:52 +0900

    janken

---
+++ src/main/java/com/example/demo/controllers/JankenController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+public class JankenController {
+    
+    @GetMapping("/demo")
+    public String playJunken(@RequestParam(name = "hand", required = false) String userHand,Model model){
+            String[] hands = {"グー","チョキ","パー"};
+            Random rand = new Random();
+            String aiHand = hands[rand.nextInt(3)];
+            String result ;
+
+            if(userHand == null){
+                result = "手を選んでください";
+            }else if(userHand.equals(aiHand)){
+                result = "あいこ！";
+            }else if((userHand.equals("グー")  && aiHand.equals("チョキ")) ||
+                     (userHand.equals("チョキ") && aiHand.equals("パー"))  ||
+                     (userHand.equals("パー")   && aiHand.equals("グー"))){
+                        result = "あなたの勝ちです！！やったね！";
+            }else{
+                        result = "あなたの負け..." ;
+            }
+
+            model.addAttribute("userHand",userHand);
+            model.addAttribute("aiHand",aiHand);
+            model.addAttribute("result", result);
+
+            return "Janken"; //Janken.html　を表示
+
+    }
+
+}
